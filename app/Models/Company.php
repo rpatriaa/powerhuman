fba648994b6ff3b5d4a95ab312c94f955d926054
Rizki Patria Linaldi <rpatria04@gmail.com>
2025-07-06 20:06:44 +0700

    feat: Tambahkan ResponseFormatter dan perbarui model dengan SoftDeletes serta relasi baru

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'logo',
+    ];
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function team()
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function role()
+    {
+        return $this->hasMany(Role::class);
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'company_id',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function responsibility()
+    {
+        return $this->hasMany(Responsibility::class);
+    }
+
+    public function employee()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
